cc2147400bdaa657091327f0d0b93aec85b41209
Amon Gathoka <waitaamon@gmail.com>
2018-09-21 17:13:45 +0300

    active route style

--- resources/views/admin/layouts/partials/_navigation.blade.php
+++ resources/views/admin/layouts/partials/_navigation.blade.php
@@ -3,26 +3,26 @@
 
     <ul class="menu-list">
         <li>
-            <a href="{{ route('message.index') }}">Messages</a>
+            <a href="{{ route('message.index') }}" class="{{ Route::currentRouteName() === 'message.index' || Route::currentRouteName() === 'message.create' || Route::currentRouteName() === 'message.show' ? 'is-active' : '' }}">Messages</a>
         </li>
         <li>
-            <a href="{{ route('client.index') }}">Clients</a>
+            <a href="{{ route('client.index') }}" class="{{ Route::currentRouteName() === 'client.index' || Route::currentRouteName() === 'client.create' || Route::currentRouteName() === 'client.edit' ? 'is-active' : '' }}">Clients</a>
         </li>
         <li>
-            <a href="{{ route('station.index') }}">Station</a>
+            <a href="{{ route('station.index') }}" class="{{ Route::currentRouteName() === 'station.index' || Route::currentRouteName() === 'station.create' || Route::currentRouteName() === 'station.edit'? 'is-active' : '' }}">Station</a>
         </li>
         <li>
-            <a href="{{ route('manager.index') }}">Managers</a>
+            <a href="{{ route('manager.index') }}" class="{{ Route::currentRouteName() === 'manager.index' || Route::currentRouteName() === 'manager.create' || Route::currentRouteName() === 'manager.edit' ? 'is-active' : '' }}">Managers</a>
         </li>
     </ul>
 
     <p class="menu-label">General</p>
     <ul class="menu-list">
         <li>
-            <a href="{{ route('admin.show') }}">Account Details</a>
+            <a href="{{ route('admin.show') }}" class="{{ Route::currentRouteName() === 'admin.show' }}">Account Details</a>
         </li>
         <li>
-            <a href="{{ route('admin.change.password') }}">Change Password</a>
+            <a href="{{ route('admin.change.password') }}" class="{{ Route::currentRouteName() === 'admin.change.password' ? 'is-active' : '' }}">Change Password</a>
         </li>
     </ul>
 
